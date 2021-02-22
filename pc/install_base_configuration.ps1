e10fc12def099c6b3dfc4c2e20bcc6bab43d8889
Mark Koelle <mkoelle@gmail.com>
2021-02-21 21:37:37 -0600

    add kindle reader, remove unused commands

--- pc/install_base_configuration.ps1
+++ pc/install_base_configuration.ps1
@@ -8,9 +8,12 @@
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
 # Install-BoxstarterPackage C:\src\github.com\mkoelle\template-computer-setup-master\pc\install_base_configuration.ps1
 
-#windows preferences
-Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar
-Set-TaskbarOptions -Size Small -Lock -Dock Bottom
+# This has to be updated with the script location for supporting files to be found (bloatware list)
+$scriptLocation="C:\src\github.com\mkoelle\template-computer-setup-master\pc"
+
+# Windows preferences (commands seem to be removed)
+# Set-WindosExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+# Set-TaskbarOptions -Size Small -Lock -Dock Bottom
 
 #Install basic needs
 choco install 7zip
@@ -26,15 +29,16 @@ choco install powershell-core
 choco install vscode --params "/NoDesktopIcon"
 choco install windirstat
 choco install yq
+choco install kindle
 
 #Remove the default apps
-$bloatware= Join-Path -Path $MyInvocation.MyCommand.Path -ChildPath "bloatware.txt"
+$bloatware="$scriptLocation\bloatware.txt"
 foreach($app in Get-Content $bloatware) {
   Write-Output "Removing $app"
   Get-AppxPackage "*$app*" | Remove-AppxPackage
 }
 
-# Pin items to taskbar
+# Pin items to taskbar  (commands seem to be removed)
 # Install-ChocolateyPinnedTaskBarItem -TargetFilePath $env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe
 # taskbar pinning unavialable for now https://github.com/chocolatey/choco/issues/627
 
