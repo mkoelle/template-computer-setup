8108544d3dca310b10a8a635669a255f232ce82a
Mark Koelle <mkoelle@gmail.com>
2021-12-07 21:14:48 -0600

    first pass at installer

---
+++ debian/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt install \
+  awscli \
+  bash-completion \
+  curl \
+  docker \
+  git \
+  golang \
+  jq \
+  packer \
+  thefuck \
+  tree \
+  zsh \
+  # EOL
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
