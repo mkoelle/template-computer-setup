cb5c28e592a66773a55dc63c71fc89742baa2ca7
Mark Koelle <mkoelle@gmail.com>
2022-01-10 23:43:27 -0600

    update profile

--- common/install_powershell_tools.ps1
+++ common/install_powershell_tools.ps1
@@ -1,5 +1,7 @@
 Install-Module posh-git -Scope CurrentUser -Force
 Install-Module InvokeBuild -Scope CurrentUser -Force
+Install-Module oh-my-posh -Scope CurrentUser -Repository PSGallery -Force
+Install-Module Terminal-Icons -Scope CurrentUser -Repository PSGallery -Force
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1'))
 
 function addLineToProfile {
@@ -14,7 +16,11 @@ function addLineToProfile {
 }
 
 addLineToProfile("Import-Module posh-git")
+addLineToProfile("Import-Module oh-my-posh")
+addLineToProfile("Set-PoshPrompt -Theme powerlevel10k_rainbow")
+addLineToProfile("Import-Module Terminal-Icons -Scope Local -Force")
 addLineToProfile("Set-Alias -Name fy -Value fuck!")
 
-$Personal_cert = $(Join-Path $home .ssh/personal_github)
-ssh-keygen -t ed25519 -C "mkoelle@gmail.com" -f $Personal_cert -q -N """"
+# $Personal_cert = $(Join-Path $home .ssh/personal_github)
+# ssh-keygen -t ed25519 -C "mkoelle@gmail.com" -f $Personal_cert -q -N """"
+# install https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
