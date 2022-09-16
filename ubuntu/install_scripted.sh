e771346985f1f9154b4fdff46bb304459a756b81
Mark Koelle <mkoelle@gmail.com>
2022-09-15 23:10:26 -0500

    add awscli

--- ubuntu/install_scripted.sh
+++ ubuntu/install_scripted.sh
@@ -18,3 +18,8 @@ export NVM_DIR="$HOME/.nvm"
 
 . ~/.bashrc
 nvm install node
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf ./aws*
