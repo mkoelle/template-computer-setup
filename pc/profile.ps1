2347cb5d2874b451f376ae15823ec00a86bdfb0c
Mark Koelle <mkoelle@gmail.com>
2025-02-11 18:37:47 -0600

    Create profile.ps1

---
+++ pc/profile.ps1
@@ -0,0 +1,12 @@
+Invoke-Expression (&starship init powershell)
+
+Invoke-Expression (& { (zoxide init --cmd j powershell | Out-String) })
+
+fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+Set-Alias -Name nvm -Value fnm
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
